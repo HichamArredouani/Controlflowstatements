f23ce4660fc7fd8612bd7f5a0d7f2c2366600813
Hicham Arredouani <Hichamarredouani@hotmail.com>
2025-05-02 20:48:21 +0200

    while

---
+++ src/While.java
@@ -0,0 +1,10 @@
+public class While {
+    public static void main (String[] args) {
+        int count = 0;
+
+        while (count < 42) {
+            System.out.println("Hello world");
+        }
+        }
+    }
+
--- src/examScore.java
+++ src/examScore.java
@@ -1,2 +1,16 @@
+import java.util.Scanner;
+
 public class examScore {
+    public static void main (String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter your examscore (0-100): ");
+        int score = input.nextInt();
+
+        if (score > 50) {
+                System.out.println("Gebuisd!!!!!!");
+        } else {
+            System.out.println("Geslaagd!!!!!");
+        }
+    }
 }
