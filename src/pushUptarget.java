44ab833f255f3a920375d449e22b0987bf3d8650
Hicham Arredouani <Hichamarredouani@hotmail.com>
2025-05-03 20:38:36 +0200

    While

---
+++ src/pushUptarget.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class pushUptarget {
+    public static void main (String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("How many push ups do you want to do?");
+        int target = input.nextInt();
+
+
+        int i = 1;
+        while (i  <= target) {
+            System.out.println("Doing push-up " + i + " out of " + target);
+            i++;
+        }
+
+
+    }
+}
