ea36b232d044921e01659ba2c7ed87f9b2629865
Hicham Arredouani <Hichamarredouani@hotmail.com>
2025-05-04 17:31:39 +0200

    MultiplyMethod

---
+++ src/Multiply.java
@@ -0,0 +1,10 @@
+public class Multiply {
+    public static void main (String[] args) {
+        int result = multiply(3, 5);
+        System.out.println(result);
+    }
+    public static int multiply (int a, int b) {
+        int product = a * b;
+        return product;
+    }
+}
---
+++ src/greetMethod.java
@@ -0,0 +1,14 @@
+public class greetMethod {
+    public static void main (String[] args) {
+        greet("Hicham");
+        greet("Tom");
+        greet("Zakaria");
+        greet("Adam");
+
+    }
+
+    public static void greet (String name){
+    System.out.println("Hello " + name);
+    System.out.println("Welkome to java");
+    }
+}
