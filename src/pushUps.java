8ae408d5f52d864bc0f4369f1b4ff7c2c832e450
Hicham Arredouani <Hichamarredouani@hotmail.com>
2025-05-04 16:42:12 +0200

    Doing pushups

--- src/pushUps.java
+++ src/pushUps.java
@@ -1,2 +1,18 @@
+import java.util.Scanner;
+
 public class pushUps {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("How many push-ups do you want to do? " );
+        int target = input.nextInt();
+
+
+
+        for (int i = 1; i <= target; i++) {
+            System.out.println("Doing push-up " + i + " out of " + target);
+
+        }
+
+    }
+
 }
