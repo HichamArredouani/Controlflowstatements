0fcd6e66a446780653152837ba562d94b309cf21
Hicham Arredouani <Hichamarredouani@hotmail.com>
2025-05-04 17:17:04 +0200

    inzicht

---
+++ src/Inzicht.java
@@ -0,0 +1,11 @@
+public class Inzicht {
+    public static void main (String[] args) {
+        System.out.println("The multiples of 3 between 0 - 100: ");
+
+
+
+        for (int i = 0; i  <= 100; i += 3) {
+        System.out.println(i);
+        }
+    }
+}
